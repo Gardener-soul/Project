c178e0251882ed7823c22961bb57dbd1e8f8bb3c
gardener <str-pes@naver.com>
2022-11-05 18:01:25 +0900

    Movie Star Maker , Alonememo

---
+++ PYTHON/hello.py
@@ -0,0 +1,13 @@
+people = [{'name': 'bob', 'age': 20}, 
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
+def get_age(target):
+
+    for person in people :
+        if person['name']  == target :
+            return person['age']
+    return '해당하는 이름이 없습니다.'
+
+print(get_age('john'))
